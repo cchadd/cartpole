dabe7af4bc9158927af55319b5a0dfa01fc51474
cchadd <clement.chadebec@mines-paristech.fr>
2019-12-16 12:37:42 +0100

    actor

---
+++ models/actor_net.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ActorNetwork(nn.Module):
+
+    def __init__(self, input_size, hidden_size, action_size):
+        super(ActorNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, action_size)
+
+    def forward(self, x):
+        out = F.relu(self.fc1(x))
+        out = F.relu(self.fc2(out))
+        out = F.softmax(self.fc3(out), dim=-1)
+        return out
+    
+    def select_action(self, x):
+        return torch.multinomial(self(x), 1).detach().numpy()
